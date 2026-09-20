d0480092fdffbac8694bc6a3943270065cfc0123
Harsha Vardhan <sharshareddy34@gmail.com>
2026-09-20 22:28:05 +0530

    Delete Employee directory

--- Employee/src/main/java/com/normal/Repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.normal.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.normal.Model.Employee;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Integer>{
-
-}
