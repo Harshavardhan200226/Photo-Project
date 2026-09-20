71869a570b83457a761de4630725664ebd3defb5
Harsha Vardhan <sharshareddy34@gmail.com>
2026-09-20 09:34:11 +0530

    Delete PhotoSharingProject directory

--- PhotoSharingProject/src/main/java/com/normal/repository/EventRepository.java
+++
@@ -1,6 +0,0 @@
-package com.normal.repository;
-import com.normal.entity.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
